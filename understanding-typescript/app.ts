db392d9f3523199cf856c78bc1a70cb5eca8d2f6
Marcelo Mendes <marcelopwmendes@gmail.com>
2022-05-24 23:22:38 +0100

    Working with string and boolean

--- understanding-typescript/app.ts
+++ understanding-typescript/app.ts
@@ -1,10 +1,18 @@
-function add(n1: number, n2: number) {
-    
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+
+  if (showResult) {
+    console.log(phrase + result);
+    return;
+  }
+
+  return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
